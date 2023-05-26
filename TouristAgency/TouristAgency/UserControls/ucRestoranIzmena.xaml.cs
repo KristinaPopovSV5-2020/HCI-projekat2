b8423f876e4a79b41a0aec2f14389da7334ae629
sv5020 <99079887+sv50-2020@users.noreply.github.com>
2023-05-26 12:09:04 +0200

    added dodaj i izmeni restoran

---
+++ TouristAgency/TouristAgency/UserControls/ucRestoranIzmena.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TouristAgency.Model;
+
+namespace TouristAgency.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ucRestoranIzmena.xaml
+    /// </summary>
+    public partial class ucRestoranIzmena : UserControl
+    {
+        public Restoran restoran = new Restoran();
+
+
+        public ucRestoranIzmena(string id, string naziv, string adresa, string ocena)
+        {
+            InitializeComponent();
+
+            this.id.Hint = id;
+            this.naziv.Hint = naziv;
+            this.adresa.Hint = adresa;
+            this.ocena.Hint = ocena;
+
+            restoran.Id = this.id.Hint;
+            restoran.Naziv = this.naziv.Hint;
+            restoran.Ocena = this.ocena.Hint;
+            restoran.Adresa = this.adresa.Hint;
+
+            naslov.Text = "Izmeni restoran";
+            potvrdi.Content = "Izmeni";
+            this.id.IsEnabled = false;
+
+        }
+
+        public ucRestoranIzmena()
+        {
+            InitializeComponent();
+
+            naslov.Text = "Dodaj restoran";
+            potvrdi.Content = "Dodaj";
+        }
+
+        public event EventHandler VratiSeNa_Restoran;
+        public event EventHandler<RestoranArgs> Dodaj_Restoran;
+        public event EventHandler<RestoranArgs> Izmeni_Restoran;
+
+        private void Vrati_Se(object sender, RoutedEventArgs e)
+        {
+            VratiSeNa_Restoran?.Invoke(this, EventArgs.Empty);
+
+        }
+
+        private void Potvrdi(object sender, RoutedEventArgs e)
+        {
+            RestoranArgs args = new RestoranArgs();
+
+            restoran.Id = id.Hint;
+            restoran.Naziv = naziv.Hint;
+            restoran.Ocena = ocena.Hint;
+            restoran.Adresa = adresa.Hint;
+
+            args.PovratnaVrednost = restoran;
+
+            Button b = (Button)sender;
+            if (b.Content.ToString() == "Dodaj")
+            {
+                Dodaj_Restoran?.Invoke(this, args);
+            }
+            else
+            {
+                Izmeni_Restoran?.Invoke(this, args);
+                MessageBox.Show($"Restoran '{restoran.Id}' je izmenjen.", "Restoran izmenjen", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+
+
+
+        }
+    }
+}
--- TouristAgency/TouristAgency/UserControls/ucRestorani.xaml.cs
+++ TouristAgency/TouristAgency/UserControls/ucRestorani.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +21,12 @@ namespace TouristAgency.UserControls
     /// </summary>
     public partial class ucRestorani : UserControl
     {
-        ObservableCollection<Restoran> restorani = new ObservableCollection<Restoran>();
+        ObservableCollection<Restoran> restorani;
         public ucRestorani()
         {
             InitializeComponent();
+
+            restorani = new ObservableCollection<Restoran>();
 
             restorani.Add(new Restoran("1", "Naziv1", "Lokacija1","5"));
             restorani.Add(new Restoran("1", "Naziv1", "Lokacija1", "5"));
@@ -51,6 +54,69 @@ namespace TouristAgency.UserControls
                 }
 
             }
+
+        }
+
+        private void Izmeni_Restoran(object sender, RoutedEventArgs e)
+        {
+
+            Restoran selectedItem = restoraniDataGrid.SelectedItem as Restoran;
+
+            ucRestoranIzmena forma = new ucRestoranIzmena(selectedItem.Id, selectedItem.Naziv, selectedItem.Adresa, selectedItem.Ocena);
+            mainComponent.Children.Clear();
+            mainComponent.Children.Add(forma);
+
+            forma.Izmeni_Restoran += Promeni;
+            forma.VratiSeNa_Restoran += Vrati;
+
+
+
+
+        }
+
+        private void Dodaj_Restoran(object sender, RoutedEventArgs e)
+        {
+            ucRestoranIzmena forma = new ucRestoranIzmena();
+            mainComponent.Children.Clear();
+            mainComponent.Children.Add(forma);
+
+            forma.Dodaj_Restoran += Ucitaj;
+            forma.VratiSeNa_Restoran += Vrati;
+
+
+        }
+
+        private void Ucitaj(object sender, RestoranArgs e)
+        {
+            ucRestorani atr = new ucRestorani();
+            atr.restorani.Add(e.PovratnaVrednost);
+            mainComponent.Children.Clear();
+            mainComponent.Children.Add(atr);
+
+
+        }
+
+        private void Promeni(object sender, RestoranArgs e)
+        {
+            ucRestorani atr = new ucRestorani();
+            Restoran restoranZaPromenu = atr.restorani.FirstOrDefault(item => item.Id == e.PovratnaVrednost.Id);
+            restoranZaPromenu.Id = e.PovratnaVrednost.Id;
+            restoranZaPromenu.Naziv = e.PovratnaVrednost.Naziv;
+            restoranZaPromenu.Ocena = e.PovratnaVrednost.Ocena;
+            restoranZaPromenu.Adresa = e.PovratnaVrednost.Adresa;
+
+            mainComponent.Children.Clear();
+            mainComponent.Children.Add(atr);
+
+
+        }
+
+        private void Vrati(object sender, EventArgs e)
+        {
+            ucRestorani atr = new ucRestorani();
+            mainComponent.Children.Clear();
+            mainComponent.Children.Add(atr);
+
 
         }
     }
